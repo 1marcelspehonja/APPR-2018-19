d60468ab610ab5d89bd4e0f3c86bd63a2d6cf351
Marcel Spehonja <spehonja.marcel@gmail.com>
2020-08-14 18:50:32 +0200

    uvoz in grafi

--- uvoz/uvoz.r
+++ uvoz/uvoz.r
@@ -8,14 +8,13 @@ library(maptools)
 library(reshape2)
 library(ggplot2)
 
-
 # 2. faza: Uvoz podatkov
 
 # Funkcija, ki uvozi podatke o BDP iz csv dokumenta
 podatki.gdp <- read_csv("podatki/GDP_PPP.csv", skip=5, col_names=c('Drzava', 'Kratica', 'GDP PPP ($)', 'Koda', 1960:2019), 
                         na=":",locale=locale(encoding="Windows-1250"))
 
-podatki.gdp <- podatki.gdp %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(11:21)
+podatki.gdp <- podatki.gdp %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(3:21)
 
 podatki.gdp <- melt(podatki.gdp, id.vars=c('Drzava', 'Kratica'), measure.vars=3:21, value.name='GDP PPP ($)', variable.name='Leto', 
                     na.rm=FALSE) %>% arrange(Drzava)
@@ -26,9 +25,9 @@ podatki.gdp <- melt(podatki.gdp, id.vars=c('Drzava', 'Kratica'), measure.vars=3:
 podatki.rodnost <- read_csv("podatki/število_rojstev_na_1000.csv", skip=5, col_names=c('Drzava', 'Kratica', 'St. rojstev na 1000', 'Koda', 1960:2019), 
                                na=":",locale=locale(encoding="Windows-1250"))
 
-podatki.rodnost <- podatki.rodnost %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(11:21)
+podatki.rodnost <- podatki.rodnost %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(3:21)
 
-podatki.rodnost <- melt(podatki.rodnost, id.vars=c('Drzava', 'Kratica'), measure.vars=3:21, value.name='St. rojstev na 1000 preb.', variable.name='Leto', 
+podatki.rodnost <- melt(podatki.rodnost, id.vars=c('Drzava', 'Kratica'), measure.vars=3:21, value.name='Rodnost', variable.name='Leto', 
                                  na.rm=FALSE) %>% arrange(Drzava)
 
 
@@ -37,7 +36,7 @@ podatki.rodnost <- melt(podatki.rodnost, id.vars=c('Drzava', 'Kratica'), measure
 podatki.zivlj.doba <- read_csv("podatki/pricakovana_zivlj_doba.csv", skip=5, col_names=c('Drzava', 'Kratica', 'Pricakovana zivlj. doba', 'Koda', 1960:2019), 
                             na=":",locale=locale(encoding="Windows-1250"))
 
-podatki.zivlj.doba <- podatki.zivlj.doba %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(11:21)
+podatki.zivlj.doba <- podatki.zivlj.doba %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(3:21)
 
 podatki.zivlj.doba <- melt(podatki.zivlj.doba, id.vars=c('Drzava', 'Kratica'), measure.vars=3:21, value.name='Pricakovana zivljenjska doba', variable.name='Leto', 
                         na.rm=FALSE) %>% arrange(Drzava)
@@ -48,7 +47,7 @@ podatki.zivlj.doba <- melt(podatki.zivlj.doba, id.vars=c('Drzava', 'Kratica'), m
 podatki.umrljivost.novorojenckov <- read_csv("podatki/umrljivost_novorojenckov_na_1000.csv", skip=5, col_names=c('Drzava', 'Kratica', 'St. smrti novorojenckov na 1000 rojstev', 'Koda', 1960:2019), 
                             na=":",locale=locale(encoding="Windows-1250"))
 
-podatki.umrljivost.novorojenckov <- podatki.umrljivost.novorojenckov %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(11:21)
+podatki.umrljivost.novorojenckov <- podatki.umrljivost.novorojenckov %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(3:21)
 
 podatki.umrljivost.novorojenckov <- melt(podatki.umrljivost.novorojenckov, id.vars=c('Drzava', 'Kratica'), measure.vars=3:21, value.name='Umrljivost novorojenckov na 1000 rojstev', variable.name='Leto', 
                            na.rm=FALSE) %>% arrange(Drzava)
@@ -74,6 +73,8 @@ podatki.splavi <- podatki.splavi %>% filter(Leto >= 2000, Leto <= 2018, Kratica 
   
 podatki.splavi <- podatki.splavi %>% select('Kratica', 'Leto', 'St. splavov na leto')
 
+podatki.splavi$Leto <- as.factor(podatki.splavi$Leto)
+
 
 
 # Funkcija, ki uvozi podatke o glavni religiji posamezne države
@@ -81,18 +82,67 @@ podatki.religija <- read_csv("podatki/main-religion-of-the-country-in.csv", skip
                            na=":",locale=locale(encoding="Windows-1250"))
 
 
+# Funkcija, ki uvozi podatke o celinah za posamezno državo
+podatki.celine <- read_delim("podatki/celine.csv", delim=",", skip=1, col_names=c('Celina', 'Drzava'), locale=locale(encoding="Windows-1250"))
+podatki.celine <- podatki.celine[, c(2,1)]
 
+# Funkcija, ki uvozi podatke o številu prebivalcev
+podatki.prebivalci <- read_csv("podatki/prebivalstvo.csv", skip=5, col_names=c('Drzava', 'Kratica', 'Prebivalci', 'Koda', 1960:2019), 
+                               na=":",locale=locale(encoding="Windows-1250"))
+podatki.prebivalci <- podatki.prebivalci %>% select('Drzava', 'Kratica', 45:63) %>% drop_na(11:21)
+podatki.prebivalci <- melt(podatki.prebivalci, id.vars=c('Drzava', 'Kratica'), measure.vars=3:21, value.name='Prebivalci', variable.name='Leto', 
+                          na.rm=FALSE) %>% arrange(Drzava)
+
+#____________________________________________________________________________________________
 #Združimo v eno tabelo
-zdruzeni.podatki <- inner_join(podatki.gdp, select(podatki.rodnost, c(1,3,4)), by = c('Drzava', 'Leto')) %>% 
+zdruzeni.podatki <- inner_join(podatki.gdp, select(podatki.prebivalci, c(1,3,4)), by = c('Drzava', 'Leto')) %>% 
+  inner_join(., select(podatki.rodnost, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
   inner_join(., select(podatki.zivlj.doba, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
   inner_join(., select(podatki.umrljivost.novorojenckov, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
   left_join(., select(podatki.izobrazba, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
+  left_join(., podatki.splavi, by=c('Kratica', 'Leto')) %>%
   left_join(., select(podatki.religija, c(1,4)), by=c('Drzava')) %>%
-  left_join(., podatki.splavi, by=c('Kratica', 'Leto')) 
+  left_join(., podatki.celine, by='Drzava') 
 
-zdruzeni.podatki %>% filter(is.na(Religija)) %>% View
+religija.povprecje <- zdruzeni.podatki %>% group_by(Religija, Leto) %>% 
+  summarise('Povprecje BDP'=mean(`GDP PPP ($)`), 'Povprecje rodnost'=mean(`St. rojstev na 1000 preb.`)) %>% 
+  drop_na()
+
+celina.povprecje <- zdruzeni.podatki %>% group_by(Celina, Leto) %>% 
+  summarise('Povprecje BDP'=mean(`GDP PPP ($)`), 'Povprecje rodnost'=mean(`St. rojstev na 1000 preb.`)) %>% 
+  drop_na()
 
 
+
+zdruzeni.podatki.krajse <- inner_join(podatki.gdp, select(podatki.prebivalci, c(1,3,4)), by = c('Drzava', 'Leto')) %>% 
+  inner_join(., select(podatki.rodnost, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
+  inner_join(., select(podatki.zivlj.doba, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
+  inner_join(., select(podatki.umrljivost.novorojenckov, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
+  left_join(., select(podatki.izobrazba, c(1,3,4)), by=c('Drzava', 'Leto')) %>%
+  left_join(., podatki.splavi, by=c('Kratica', 'Leto')) %>%
+  inner_join(., select(podatki.religija, c(1,4)), by=c('Drzava')) %>%
+  inner_join(., podatki.celine, by='Drzava') 
+
+
+#zdruzeni.podatki %>% filter(is.na(Religija)) %>% View
+zdruzeni.podatki %>% filter(Leto==2018) %>% View
+
+
+#____________________________________________________________________________________________
+sapply(podatki.gdp, class)
+sapply(podatki.rodnost, class)
+sapply(podatki.umrljivost.novorojenckov, class)
+sapply(podatki.zivlj.doba, class)
+sapply(podatki.izobrazba, class)
+sapply(podatki.religija, class)
+sapply(podatki.splavi, class)
+sapply(podatki.celine, class)
+
+sapply(zdruzeni.podatki, class)
+
+remove(podatki.gdp, podatki.izobrazba, podatki.rodnost, podatki.splavi, 
+       podatki.umrljivost.novorojenckov, podatki.zivlj.doba)
+remove(podatki.prebivalci, podatki.celine, podatki.religija)
 
 # # Funkcija, ki uvozi podatke o številu novorojenih otrok iz csv dokumenta
 # podatki.otroci.na.zensko <- read_csv("podatki/št_otrok_na_žensko.csv", skip=5, 
--- vizualizacija/vizualizacija.r
+++ vizualizacija/vizualizacija.r
@@ -1,13 +1,76 @@
+library(dplyr)
+library(tidyr)
+library(rgdal)
+library(rgeos)
+library(mosaic)
+library(maptools)
+library(reshape2)
+library(ggplot2)
+
 # 3. faza: Vizualizacija podatkov
 
-# Uvozimo zemljevid.
-zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
-                             pot.zemljevida="OB", encoding="Windows-1250")
-levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
-  { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
-zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
-zemljevid <- fortify(zemljevid)
+# # Uvozimo zemljevid.
+# zemljevid <- uvozi.zemljevid("http://baza.fmf.uni-lj.si/OB.zip", "OB",
+#                              pot.zemljevida="OB", encoding="Windows-1250")
+# levels(zemljevid$OB_UIME) <- levels(zemljevid$OB_UIME) %>%
+#   { gsub("Slovenskih", "Slov.", .) } %>% { gsub("-", " - ", .) }
+# zemljevid$OB_UIME <- factor(zemljevid$OB_UIME, levels=levels(obcine$obcina))
+# zemljevid <- fortify(zemljevid)
+# 
+# # Izračunamo povprečno velikost družine
+# povprecja <- druzine %>% group_by(obcina) %>%
+#   summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
 
-# Izračunamo povprečno velikost družine
-povprecja <- druzine %>% group_by(obcina) %>%
-  summarise(povprecje=sum(velikost.druzine * stevilo.druzin) / sum(stevilo.druzin))
+
+#graf1
+zdruzeni.podatki.krajse1 <- filter(zdruzeni.podatki.krajse,Leto==2018)[sample(nrow(filter(zdruzeni.podatki.krajse,Leto==2018)), 40), ]
+
+graf1 <- dotchart(filter(arrange(zdruzeni.podatki.krajse1,Rodnost), Leto==2018)$Rodnost,
+                  labels=filter(arrange(zdruzeni.podatki.krajse1,Rodnost), Leto==2018)[,1], cex=.7, 
+                  main="Rodnost", xlab="Stevilo rojstev na 1000 prebivalcev")
+remove(zdruzeni.podatki.krajse1)
+
+#graf2
+zdruzeni.podatki.krajse2 <- filter(zdruzeni.podatki.krajse,Leto==2000)[sample(nrow(filter(zdruzeni.podatki.krajse,Leto==2018)), 40), ]
+
+graf2 <- dotchart(filter(arrange(zdruzeni.podatki.krajse2,Rodnost), Leto==2000)$Rodnost,
+                  labels=filter(arrange(zdruzeni.podatki.krajse2,Rodnost), Leto==2000)[,1], cex=.7, 
+                  main="Rodnost", xlab="Stevilo rojstev na 1000 prebivalcev")
+remove(zdruzeni.podatki.krajse2)
+
+
+#graf3 
+zdruzeni.podatki$Leto <- as.numeric(as.character(zdruzeni.podatki$Leto))
+graf3 <- ggplot(zdruzeni.podatki %>% filter(Drzava %in% c('Slovenia',
+                                                          'Germany',
+                                                          'China', 
+                                                          'Japan',
+                                                          'Nigeria', 
+                                                          'Iraq',
+                                                          'South Africa',
+                                                          'India')),
+                aes(x=Leto,y=Rodnost,color=Drzava)) +
+  geom_point(shape=21, size=2.5, fill= "black") +
+  geom_line(size = 1.2) + 
+  theme_classic() +
+  labs(x="Leto", y ="Stevilo rojstev na 1000 prebivalcev", title ="Rodnost") +
+  theme(axis.title=element_text(size=11), plot.title=element_text(size=15, hjust=0.5)) + 
+  scale_colour_manual(values=c("dodgerblue3","purple","springgreen4","black","orangered3","dark grey","yellow", "tan1"), 
+                    name="Drzava",
+                    breaks=c('Slovenia', 'Germany', 'China', 'Japan', 'Nigeria',  'Iraq', 'South Africa', 'India'),
+                    labels=c('Slovenia', 'Germany', 'China', 'Japan', 'Nigeria', 'Iraq', 'South Africa', 'India'))
+  
+#graf4
+religija.povprecje$Leto <- as.numeric(as.character(religija.povprecje$Leto))
+graf4 <- ggplot(religija.povprecje,
+                aes(x=Leto, y=religija.povprecje$"Povprecje rodnost", color=Religija)) +
+  geom_point(shape=1, size=2.5, fill= "black") +
+  geom_line(size = 1.2) + 
+  theme_bw() +
+  labs(x="Leto", y ="Povprecno stevilo rojstev na 1000 prebivalcev", title ="Rodnost glede na vero") +
+  scale_colour_manual(values=c("dodgerblue3","purple","springgreen4","black","tan1","dark grey","maroon"))
+
+
+#graf5
+
+
