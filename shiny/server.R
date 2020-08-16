660d877f5d00db5687a3d9b8ba1a26f40cdeeb22
Marcel Spehonja <spehonja.marcel@gmail.com>
2020-08-17 01:22:09 +0200

    spremenil shiny - ustvaril zemljevid

--- shiny/server.R
+++ shiny/server.R
@@ -1,10 +1,12 @@
 library(shiny)
+library(dplyr)
+library(ggplot2)
 
-shinyServer(function(input, output) {
-  output$rodnost <- DT::renderDataTable({
-    zdruzeni.podatki %>% spread(key="Leto", value="Rodnost") %>%
-      rename(`Država`=Drzava)
-  })
+# shinyServer(function(input, output) {
+#   output$rodnost <- DT::renderDataTable({
+#     zdruzeni.podatki %>% spread(key="Leto", value="Rodnost") %>%
+#       rename(`Država`=Drzava)
+#   })
   
   # output$pokrajine <- renderUI(
   #   selectInput("pokrajina", label="Izberi pokrajino",
@@ -21,4 +23,23 @@ shinyServer(function(input, output) {
   #   ggplot(t, aes(x=naselja)) + geom_histogram() +
   #     ggtitle(main) + xlab("Število naselij") + ylab("Število občin")
   # })
-})
+# })
+
+function(input, output) 
+{
+  
+  output$zemljevid1 <- renderPlot({
+    ggplot() +
+      geom_polygon(data=left_join(zemljevid, rodnost.vse %>% filter(Leto == input$letnica), by=c("NAME"="Drzava")),
+                   aes(x=long, y=lat, group=group, fill=Rodnost), color = "black") + 
+      coord_cartesian(xlim=c(-150, 170), ylim=c(-50, 80)) +
+      scale_fill_gradient2(low ="yellow", mid = "red", high = "blue", 
+                           na.value = "white", name="Rodnost",
+                           guide = "legend",
+                           midpoint=25) +
+      xlab("") + ylab("") + ggtitle("Rodnost po državah sveta") +
+      theme(plot.title = element_text(hjust = 0.5))
+    
+  })
+  
+}
--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,26 @@
 library(shiny)
 
-shinyUI(fluidPage(
-  
-  titlePanel("Slovenske občine"),
-  
-   tabsetPanel(
-       tabPanel("Rodnost",
-                DT::dataTableOutput("rodnost"))
+# shinyUI(fluidPage(
+#   
+#   titlePanel("Slovenske občine"),
+#   
+#    tabsetPanel(
+#        tabPanel("Rodnost",
+#                 DT::dataTableOutput("rodnost"))
   #     
   #     tabPanel("Število naselij",
   #              sidebarPanel(
   #                 uiOutput("pokrajine")
   #               ),
   #              mainPanel(plotOutput("naselja")))
-     )
-))
+#      )
+# ))
+
+
+fluidPage(
+  tabPanel("Zemljevid",
+           sidebarPanel(
+             selectInput("letnica", label = "Leto", 
+                         choices = unique(rodnost.vse$Leto)), width=2),
+           mainPanel(plotOutput("zemljevid1"), width=10))
+)
